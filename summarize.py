3466abe59cddbd0149897d27f7204151269407a3
JDNafz <nafziger10@gmail.com>
2025-03-02 18:35:45 -0600

    added a summarize doc to start making calcs without http calls

---
+++ summarize.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# Read the CSV file
+file_path = r'data_scraped\Point_DF_13758-13759_2025-03-02.csv'
+data = pd.read_csv(file_path)
+
+# Display the first few rows of the dataframe
+print(data.head())
+
+# Sum up the competition points from all the events
+total_points = data['points'].sum()
+# Sum up the competition points from all the events, sorted by level
+total_points_by_level = data.groupby('level')['points'].sum().sort_values(ascending=False)
+print(total_points_by_level)
